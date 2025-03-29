6e7aa683120ef884663083aec94bf1dd15dc37e3
Sathyan <sathyan.kesavan@kasadara.com>
2025-03-29 15:22:11 +0530

    Add project files.

---
+++ Sathyan-Shop.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sathyan_Shop.Web.Models;
+
+namespace Sathyan_Shop.Web.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category>categories { get; set; }
+    }
+}
---
+++ Sathyan-Shop.Web/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Sathyan_Shop.Web.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
+    
